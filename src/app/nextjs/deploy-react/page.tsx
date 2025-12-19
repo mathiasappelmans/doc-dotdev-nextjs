5fe4741a9ed7b8b3c6f999ed8969cbe14ddbe6de
Mathias Appelmans <math.appelmans@gmail.com>
2025-12-19 13:15:04 +0100

    fix

--- src/app/nextjs/deploy-react/page.tsx
+++ src/app/nextjs/deploy-react/page.tsx
@@ -1,15 +1,23 @@
 import CodeBlock from "@/app/components/CodeBlock";
-import { Typography} from "@mui/material";
+import { Link, Typography} from "@mui/material";
 import Image from "next/image";
 
-const page = () => { 
+const page = () => {
 
-const text = `// on local
+const text = `// upgrade node if needed
+npm cache clean -f
+npm install -g n
+n stable
+clone the_github_repo_ssh_url
+npm i
 npm run build
+
+// At first deploy, enable SSL/HTTPS with certbot
+certbot --expand -d social.dotdev.be // with your app name
 `;
 
 const text1 = `server {
-	server_name social.dotdev.be;	
+	server_name social.dotdev.be;
 
 	root 						/var/www/srv_dev/app/social/build;
 	index index.html;
@@ -37,37 +45,41 @@ server {
 }
 `;
   return (
-    
+
       <div className='overflow-x-auto text-sm'>
         <Typography variant="h6" component="div" sx={{mb:2}}>
-          ✨ Deploy a React app on Debian 11 VPS
+          ✨ Deploy a React app on Debian 12 VPS
         </Typography>
+				<Link href="https://www.digitalocean.com/community/tutorials/deploy-react-application-with-nginx-on-ubuntu" target="_blank" rel="noopener" sx={{mb:2, display:"block"}}>	
+				https://www.digitalocean.com/community/tutorials/deploy-react-application-with-nginx-on-ubuntu
+				</Link>
         <CodeBlock text={text} />
-        <Typography variant="h6" component="div" sx={{mb:2}}>
-          Connect to VPS with Filezilla
+        <Typography component="div" sx={{mb:2}}>
+					run a script that copy the local build folder to the remote server
         </Typography>
-        <Typography variant="h6" component="div" sx={{mb:2}}>
-          copy local build folder to /var/www/srv_dev/app/social/
+				<CodeBlock text={`scp -r ./build/* username@server_ip:/var/www/srv_dev/app/social/build`} />
+        <Typography component="div" sx={{mb:2}}>
+					or do it manually with Filezilla
         </Typography>
-        <Image
+				<Image
         width={0}
         height={0}
         sizes="100vw"
-        style={{width: '100%', height: 'auto'}} 
-        className="h-auto rounded-lg" 
+        className="w-full h-auto rounded-lg" 
         src="/deploy-react.png" 
         alt=""
       />
         <Typography variant="body2" component="div" sx={{mt:5, wordBreak: "break-all"}}>
-          configure /var/www/srv_dev/config/nginx/domains/social.dotdev.be.conf, restart nginx
+          configure /var/www/srv_dev/config/nginx/domains/social.dotdev.be.conf and restart nginx:
         </Typography>
         <CodeBlock text={text1} />
-        <Typography variant="body2" component="div" sx={{mt:5}}>
-          no pm2 config needed, no git on remote server
+        <CodeBlock text={`service nginx restart`} />
+        <Typography variant="body2" component="div" sx={{my:5}}>
+          Note: No pm2 config needed for React apps, only for NextJS apps.
         </Typography>
-        
+
       </div>
-   
+
   )
 }
 
